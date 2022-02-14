09299bedb73c6a64a0a2056b6c7faeda03f5c747
Imani-ES <imanimuh@buffalo.edu>
2022-02-14 16:56:52 -0500

    Edit docker file and server

--- Phase_1/server.js
+++ Phase_1/server.js
@@ -10,27 +10,24 @@ const port  = process.env.Port || 3000;
 const { MongoClient } = require("mongodb");
 const { WSATYPE_NOT_FOUND } = require('constants');
 const imani_dbPass = process.env.DB_PASS_442;
-const imani_uri = 'mongodb+srv://CSE442:' + 'CSE442cse' + '@cluster0.k7tia.mongodb.net/test';
+const imani_uri = 'mongodb+srv://'+'486'+':486'+'@cluster0.k7tia.mongodb.net/'+'databasename';
 const imani_client = new MongoClient(imani_uri,{keepAlive: 1});
 
-
-if(get_room_info()){
+chat_history = ['Welcome all'];
+/*
+if(get_chat_history()){
     console.log("Got room data");
 }
-
-app.use('/movie_room.css', express.static(__dirname + '/movie_room.css'));
-app.get("/movie_room", (req, res) => res.sendFile(__dirname + "/movie_room.html"));
+*/
+app.use('/phase_1.css', express.static(__dirname + '/phase_1.css'));
+app.get("/phase_1", (req, res) => res.sendFile(__dirname + "/index.html"));
 
  io.on("connection", function(socket) {
-  io.emit("user connected",JSON.stringify("Hello via Json",replacer));
-
-  socket.on("client", function(msg) {
-    console.log("hello from client"+msg);    
-  });
+  io.emit('chat_history', JSON.stringify(chat_history,replacer));
 
   socket.on("chat", function(msg){
     console.log("Processing chat from User: " + msg);
-    chat_history.push(["User x",msg]);
+    chat_history.push(msg);
     //find a way to push to database collection
     io.emit('chat_history', JSON.stringify(chat_history,replacer));
   });
@@ -40,14 +37,8 @@ app.get("/movie_room", (req, res) => res.sendFile(__dirname + "/movie_room.html"
 
  server.listen(port, () => console.log("listening on http://localhost:"+port));
 
- 
-async function get_room_info() {
-  //Movie Data stored as
-/* movie_name: "moviename"
-  genre:""
-  yeae:""
-  img_url:""
-*/ 
+/*
+async function get_chat_history() {
 await imani_client.connect();
 console.log("MongoDB connected");
 const db = imani_client.db("Movies");
@@ -59,6 +50,7 @@ movies.findOne({room_info:room_name},{}, function(err, result) {
 }); 
 return 0;
 }
+*/
 
 //JSON wrapper & unwrapper functions
 function replacer(key, value) {
