ba8b62331dc6fd2638e04f64482242d71c002068
Imani-ES <imanimuh@buffalo.edu>
2022-02-23 14:57:29 -0500

    Logging messages completed

--- Phase_1/App_1/server.js
+++ Phase_1/App_1/server.js
@@ -12,78 +12,62 @@ const port  = process.env.Port;
 const uri = process.env.DB_connect.toString();
 const database_name = process.env.db_name.toString();
 const app_name = process.env.app_name.toString();
-
+var chat_counter = 0;
 //Create Database
 MongoClient.connect(uri, function(err, db) {
   if (err) throw err;
   console.log("Mongo Connected");
   _db = db.db("test");
-  /*Apparently referencing the collection automatically creates it
-  _db.createCollection("chat_collection", function(err, res) {
-    if (err) throw err;
-   
-  });
-  */
-  var chat_history = {chat_history:['Welcome all']};
+  var chat_history = {id: chat_counter,chat_history:['Welcome all']};
   _db.collection("chat_collection").insertOne(chat_history, function(err, res){
     if (err) throw err;
     console.log("Collection created!");
     console.log("History element instantiated");
   })
-  //db.close();
-  //console.log("Mongo Connection closed");
+ chat_counter += 1;
 });
 
-//global variables
-chat_history = ['Welcome all'];
-db = {}
+  //global variables
+  chat_history = ['Welcome all'];
+  db = {}
 
-//files used
-app.use('/phase_1.css', express.static(__dirname + '/phase_1.css'));
+  //files used
+  app.use('/phase_1.css', express.static(__dirname + '/phase_1.css'));
 
-//send client HTML file
-app.get("/phase_1", (req, res) => res.sendFile(__dirname + "/index.html"));
+  //send client HTML file
+  app.get("/phase_1", (req, res) => res.sendFile(__dirname + "/index.html"));
 
-//socket functionality
- io.on("connection", function(socket) {
-   
-  //initial connection
-  console.log("User Connected")
-   io.emit('chat_history', JSON.stringify(chat_history,replacer));
-   
-   //messages
-   socket.on("chat", function(msg){
+  //socket functionality
+  io.on("connection", function(socket) {
+    
+    //initial connection
+    console.log("User Connected")
+    io.emit('chat_history', JSON.stringify(chat_history,replacer));
+    
+    //messages
+    socket.on("chat", function(msg){
 
-     console.log("Processing chat: " + msg);
-     chat_history.push(msg);
+      console.log("Processing chat: " + msg);
+      chat_history.push(msg);
 
-     //database connect
-     console.log("Conecting to mongo");
-     MongoClient.connect(uri, function(err, _db) {
-      assert.equal(null, err);
-      console.log("Connected to mongo");
-      var db = _db.db(database_name)
-      var chat_history = {chat_history:chat_history};
-      //check if database is empty
-      var empty = 1
-      db.chat_collection.findOne({chat_history}, function(err, result) {
-        if (err) throw err;
-        if(result){empty = 0;}
-      });
-      if(empty){
+      //database connect
+      console.log("Conecting to mongo");
+      MongoClient.connect(uri, function(err, _db) {
+        assert.equal(null, err);
+        console.log("Connected to mongo");
+        var db = _db.db(database_name)
+        var chat_history = {id:chat_counter,chat_history:chat_history};
         db.collection("chat_collection").insertOne(chat_history, function(err, res){
           assert.equal(null, err);
-          console.log("Chat history logged");
+          console.log(chat_history+" logged");
         });
-      }       
-      //db.close;
-      //console.log("Disconnected from mongo")
-    });
-     io.emit('chat_history', JSON.stringify(chat_history,replacer));
-    });
+        chat_counter += 1;
+      });
+      io.emit('chat_history', JSON.stringify(chat_history,replacer));
+      });
 
 
- });
+  });
 
 //JSON wrapper & unwrapper functions
 function replacer(key, value) {
